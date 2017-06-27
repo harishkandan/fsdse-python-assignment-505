934aec9a28da6658e371b48a66bd092d821c8fcf
harishkandan <harishkandan95@gmail.com>
2017-06-27 04:08:24 +0000

    Done

--- build.py
+++ build.py
@@ -21,12 +21,12 @@ def first_country(df):
     return df.iloc[0,:]
 
 def gold_medal(df):
-    return df['Country_name'][df['Gold.2']==df['Gold.2'].max()].values[0]
+    return df['Country_name'][df['Gold.2']==df['Gold.2'].max()][0]
 
 
 def biggest_difference_in_gold_medal(df):
-    return df['Country_name'][abs(df['Total']-df['Total.1'])==abs(df['Total']-df['Total.1']).max()].values[0]
-
+    return df['Country_name'][abs(df['Total']-df['Total.1'])==abs(df['Total']-df['Total.1']).max()][0]
 def get_points(df):
     df['points'] = (df['Gold.2']*3+df['Silver.2']*2+df['Bronze.2'])
     return df['points']
+print biggest_difference_in_gold_medal(load_data())
